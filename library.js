dc6842a9bbb5bb85239b29553d97dc8eae4c6f97
altusgerona <alecgerona@gmail.com>
2016-02-27 03:43:54 +0800

    Finish countSubstrPattern(strOriginal, strPattern) function.

--- library.js
+++ library.js
@@ -11,6 +11,22 @@ function getHammingDistance(str1, str2){
 	return diff;
 
 }
+
+function countSubstrPattern(strOriginal, strPattern){
+	var total = 0;
+	var count=0;
+	for (var i=0; i<strOriginal.length; i++){
+		for (var j=0; j<strPattern.length && i<=strOriginal.length - strPattern.length; j++){
+			if (strPattern.charAt(j) == strOriginal.charAt(i+j)) count++;
+		}
+		if (count == strPattern.length) total++;
+		count=0;
+	}
+	return total;
+}
+
+
+
 function result(res){
 	console.log(res);
 }
